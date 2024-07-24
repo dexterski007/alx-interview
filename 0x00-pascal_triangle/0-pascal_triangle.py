77f3db2148be1684bc2811155a514ce78c0f0673
dexterski007 <mbouj007@gmail.com>
2024-07-24 21:27:42 +0100

    variables?

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,16 +7,16 @@ def pascal_triangle(n):
     """creates a triangle of pascal based
     on the provided number
     """
-    new_list = []
+    triangle = []
     if type(n) is not int or n <= 0:
-        return new_list
+        return triangle
     for line in range(n):
         actual_row = []
         for row in range(line + 1):
             if row == 0 or line == row:
                 actual_row.append(1)
             elif row > 0 and line > 0:
-                actual_row.append(new_list[line - 1][row - 1]
-                                  + new_list[line - 1][row])
-        new_list.append(actual_row)
-    return new_list
+                actual_row.append(triangle[line - 1][row - 1]
+                                  + triangle[line - 1][row])
+        triangle.append(actual_row)
+    return triangle
