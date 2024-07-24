da37403de798d0f9b54154b4157fbed0f09a3264
dexterski007 <mbouj007@gmail.com>
2024-07-24 21:25:42 +0100

    commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-''' pascal triangle function '''
+''' pascal triangle module used to generate a pascal triangle
+'''
 
 
 def pascal_triangle(n):
-    ''' pascal triangle function '''
+    '''creates a triangle of pascal based
+    on the provided number
+    '''
     new_list = []
     if type(n) is not int or n <= 0:
         return new_list
@@ -14,6 +17,6 @@ def pascal_triangle(n):
                 actual_row.append(1)
             elif row > 0 and line > 0:
                 actual_row.append(new_list[line - 1][row - 1]
-                               + new_list[line - 1][row])
+                                  + new_list[line - 1][row])
         new_list.append(actual_row)
     return new_list
