28f28098b033497fdf3b24e9cf08d3ff55f3e8ff
dexterski007 <mbouj007@gmail.com>
2024-07-24 21:14:46 +0100

    foundit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""
+'''
 pascal triangle function
-"""
+'''
 
 
 def pascal_triangle(n):
-    """ pascal triangle function """
+    ''' pascal triangle function '''
     new_list = []
-    if n <= 0 or type(n) is not int:
+    if n <= 0:
         return new_list
-    for line in range(n):
-        actual_row = []
-        for row in range(line + 1):
-            if row == 0 or row == line:
-                actual_row.append(1)
-            elif line > 0 and row > 0:
-                actual_row.append(new_list[line - 1][row - 1]
+    for line in range(0, n):
+        actual_row = [1] * (line + 1)
+        for row in range(1, line):
+            actual_row[row] = (new_list[line - 1][row - 1]
                                + new_list[line - 1][row])
         new_list.append(actual_row)
     return new_list
