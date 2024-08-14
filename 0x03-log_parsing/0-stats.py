65be3d9ff28529eb7ba15aca4d365e3888ef1d0f
dexterski007 <mbouj007@gmail.com>
2024-08-14 19:19:16 +0100

    new challenge

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,50 @@
 #!/usr/bin/python3
-""" module to parse logs from stdin
 """
+log parsing
+"""
+
 import sys
 import re
 
 
-def logparser():
-    """ log parsing function """
-    pt = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-    compiled = '{}\\-{}{}{}{}\\s*'.format(pt[0], pt[1], pt[2], pt[3], pt[4])
-    statusholder = {}
-    totalsize = 0
-    status_list = [200, 301, 400, 401, 403, 404, 405, 500]
-    line_count = 0
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = re.fullmatch(compiled, line)
-            if match is not None:
-                status_code = int(match.group('status_code'))
-                filesize = int(match.group('file_size'))
-                totalsize += filesize
-                if status_code in status_list:
-                    statusholder[status_code] = statusholder.get(
-                        status_code, 0) + 1
-                    line_count += 1
-            if line_count % 10 == 0:
-                print("File size: {}".format(totalsize))
-                for k, v in sorted(statusholder.items()):
-                    print('{}: {}'.format(k, v))
-
-    except KeyboardInterrupt:
-        print("File size: {}".format(totalsize))
-        for k, v in sorted(statusholder.items()):
-            print("{}: {}".format(k, v))
-        exit
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
 
 if __name__ == "__main__":
-    """ main entry point"""
-    logparser()
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
+
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
