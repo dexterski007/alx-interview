5b284909768f652c19f0f979feec6a982fb5aa19
dexterski007 <mbouj007@gmail.com>
2024-08-14 17:34:44 +0100

    parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,19 +1,43 @@
 #!/usr/bin/python3
-""" module to determine minimum operations
+""" module to parse logs from stdin
 """
+import sys
+import re
 
 
-def minOperations(n):
-    """ function to determine minimum operations needed
-    to achieve the desired result!
-    """
-    if n <= 1:
-        return 0
-    operation = 0
-    factor = 2
-    while n > 1:
-        while n % factor == 0:
-            operation += factor
-            n //= factor
-        factor += 1
-    return operation
+def logparser():
+    """ log parsing function """
+    pattern = (
+        r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+        r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] '
+        r'("GET /projects/260 HTTP/1\.1") '
+        r'(\d+) (\d+)$'
+    )
+    compiled = re.compile(pattern)
+    statusholder = {}
+    totalsize = 0
+    status_list = [200, 301, 400, 401, 403, 404, 405, 500]
+    try:
+        for line in sys.stdin:
+            for i in range(10):
+                match = re.match(compiled, line)
+                if match is not None:
+                    status_code = int(match.group(4))
+                    filesize = int(match.group(5))
+                    totalsize += filesize
+                    if status_code in status_list:
+                        statusholder[status_code] = statusholder.get(
+                            status_code, 0) + 1
+            print("File size: {}".format(totalsize))
+            for k, v in sorted(statusholder.items()):
+                print("{}: {}".format(k, v))
+    except KeyboardInterrupt:
+        print("File size: {}".format(totalsize))
+        for k, v in sorted(statusholder.items()):
+            print("{}: {}".format(k, v))
+        exit
+
+
+if __name__ == "__main__":
+    """ main entry point"""
+    logparser()
