e09e8e94e35f998e7ca91d2580ff4983e514f956
dexterski007 <mbouj007@gmail.com>
2024-08-20 22:45:24 +0100

    cleaner

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,11 +11,11 @@ def validUTF8(data):
             if byte & 0b10000000 == 0:
                 continue
             elif byte & 0b11100000 == 0b11000000:
-                num_bytes += 1
+                num_bytes = 1
             elif byte & 0b11110000 == 0b11100000:
-                num_bytes += 2
+                num_bytes = 2
             elif byte & 0b11111000 == 0b11110000:
-                num_bytes += 3
+                num_bytes = 3
             else:
                 return False
         else:
