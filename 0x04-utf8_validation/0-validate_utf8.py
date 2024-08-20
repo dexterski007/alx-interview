b079de4c6111dc16c52e6e74756c0477780fd0e6
dexterski007 <mbouj007@gmail.com>
2024-08-20 22:40:25 +0100

    task0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" module validate utf8
+"""
+
+
+def validUTF8(data):
+    """ function to validate utf 8"""
+    num_bytes = 0
+    for byte in data:
+        if num_bytes == 0:
+            if byte & 0b10000000 == 0:
+                continue
+            if byte & 0b11100000 == 0b11000000:
+                num_bytes += 1
+            if byte & 0b11110000 == 0b11100000:
+                num_bytes += 2
+            if byte & 0b11111000 == 0b11110000:
+                num_bytes += 3
+            return False
+        
+        if num_bytes > 0:
+            if not byte & 0b11000000 == 0b10000000:
+                return False
+            num_bytes -= 1
+    if num_bytes == 0:
+        return True
+    return False
+            
