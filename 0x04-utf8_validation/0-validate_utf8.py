3037ac43f726b97a421b121944046cfca304beae
dexterski007 <mbouj007@gmail.com>
2024-08-20 22:43:03 +0100

    new fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,7 +16,8 @@ def validUTF8(data):
                 num_bytes += 2
             if byte & 0b11111000 == 0b11110000:
                 num_bytes += 3
-            return False
+            else:
+                return False
 
         if num_bytes > 0:
             if not byte & 0b11000000 == 0b10000000:
