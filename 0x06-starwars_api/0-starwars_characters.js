6408f4c380e596fcf3d52a044e78ab94d9278957
dexterski007 <mbouj007@gmail.com>
2024-09-04 20:29:46 +0100

    fixed semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
-var request = require('request');
+const request = require('request');
 const filmid = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${filmid}`;
 
 request(url, (error, response, body) => {
-  const data = JSON.parse(body);
-  const chars = data.characters;
-  chars.forEach(char => {
-    request(char, (error, response, body) => {
-      const chardata = JSON.parse(body);
-      console.log(chardata.name)
-    })
-  });
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const chars = data.characters;
+    chars.forEach(char => {
+      request(char, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const chardata = JSON.parse(body);
+          console.log(chardata.name);
+        }
+      });
+    });
+  }
 });
