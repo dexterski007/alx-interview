155f322446dce437eeaf21edce3026eb155c151b
dexterski007 <mbouj007@gmail.com>
2024-09-04 20:50:38 +0100

    fixed with proimise

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
 const filmid = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${filmid}`;
+const url = 'https://swapi-api.alx-tools.com/api';
 
-request(url, (error, response, body) => {
+request(`${url}/films/${filmid}/`, (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    const data = JSON.parse(body);
-    const chars = data.characters;
-    chars.forEach(char => {
-      request(char, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const chardata = JSON.parse(body);
-          console.log(chardata.name);
-        }
-      });
-    });
+    const chars = JSON.parse(body).characters;
+    const chararr = chars.map(
+      charurl => new Promise((resolve, reject) => {
+        request(charurl, (errorpr, resppr, bodypr) => {
+          if (errorpr) {
+            reject(errorpr);
+          }
+          if (resppr.statusCode === 200) {
+            resolve(JSON.parse(bodypr).name);
+          }
+        });
+      })
+    );
+    Promise.all(chararr)
+      .then(names => console.log(names.join('\n')))
+      .catch(errors => console.log(errors));
   }
 });
