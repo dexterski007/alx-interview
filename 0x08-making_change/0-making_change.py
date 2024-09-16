049fa7f9a8cce4bb8c50c734b5f2e3f966beac39
dexterski007 <mbouj007@gmail.com>
2024-09-16 09:47:57 +0100

    typing

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,17 +2,19 @@
 """ make change function """
 
 
-def makeChange(coins, total):
+def makeChange(coins: list[int], total: int) -> int:
     """ make change function """
     if total <= 0:
         return 0
     coin_num = 0
     sorted_list = sorted(coins, reverse=True)
+    wallet = {}
     for coin in sorted_list:
         if total == 0:
             break
         change = total // coin
         total -= change * coin
+        wallet[coin] = change
         coin_num += change
     if total == 0:
         return coin_num
