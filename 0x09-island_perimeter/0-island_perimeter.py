45b212ef5c0c2ba037ef7f817df467b00a550282
dexterski007 <mbouj007@gmail.com>
2024-09-26 00:10:12 +0100

    check

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,12 +11,16 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 count += 4
-                if grid[i - 1][j] == 1:
-                    count -= 1
-                if grid[i + 1][j] == 1:
-                    count -= 1
-                if grid[i][j - 1] == 1:
-                    count -= 1
-                if grid[i][j + 1] == 1:
-                    count -= 1
+                if i > 0:
+                    if grid[i - 1][j] == 1:
+                        count -= 1
+                if i < len(grid) - 1:
+                    if grid[i + 1][j] == 1:
+                        count -= 1
+                if j > 0:
+                    if grid[i][j - 1] == 1:
+                        count -= 1
+                if j < len(grid[i]) - 1:
+                    if grid[i][j + 1] == 1:
+                        count -= 1
     return count
