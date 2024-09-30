7be9a8d9b6fe5d773bf17a19f0e7b7d98d54dcf5
dexterski007 <mbouj007@gmail.com>
2024-09-30 23:36:25 +0100

    primes

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" prime game in python """
+
+
+def primelist(num):
+    """ fucntion to create and return prime lists"""
+    primes = [True for i in range(num + 1)]
+    p = 2
+    while (p * p <= num):
+        if (primes[p]):
+            for i in range(p * p, num + 1, p):
+                primes[i] = False
+        p += 1
+    return primes
+
+
+def isWinner(x, nums):
+    """ function to select the winner """
+    if not x or not nums:
+        return None
+    Maria = 0
+    Ben = 0
+    for num in nums:
+        primes = primelist(num)
+        if len(primes) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    else:
+        return None
